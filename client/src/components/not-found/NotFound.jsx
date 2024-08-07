ba95b653bfe619482440f9416aac5db395eefd6c
LiliaADikova <liliqdikowa@gmail.com>
2024-08-07 20:04:10 +0300

    Edits to 404 page
    Home button added

--- client/src/components/not-found/NotFound.jsx
+++ client/src/components/not-found/NotFound.jsx
@@ -1,10 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
     return (
         <div className="full_bg">
             <div className="slider_main text-align-center">
-            <div className="not_found">
-                <h2>404</h2>
-                <h1>Not Found</h1>
+                <div className="not_found text-align-center">
+                    <h2>404</h2>
+                    <h1>OOPS! Not Found</h1>
+                    <Link
+                   
+                        className="read_more"
+                        to='/'
+                    >
+                        Home
+                    </Link>
                 </div>
             </div>
         </div>
