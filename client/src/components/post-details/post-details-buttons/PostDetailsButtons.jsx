fca91e9f62148a7ab414483e50839544da48b5a1
LiliaADikova <liliqdikowa@gmail.com>
2024-08-07 14:51:39 +0300

    Custom Modal Added on Delete

--- client/src/components/post-details/post-details-buttons/PostDetailsButtons.jsx
+++ client/src/components/post-details/post-details-buttons/PostDetailsButtons.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
 import postsAPI from "../../../api/posts-api"
+import { useState } from "react";
 
 export default function PostDetailsButtons({
     postId
@@ -7,20 +8,43 @@ export default function PostDetailsButtons({
 
     const navigate = useNavigate();
 
+    const [showModal, setShowModal] = useState(false)
+
     const deleteHandler = async () => {
         console.log(postId)
         try {
             await postsAPI.remove(postId);
-            navigate('/')
+            navigate('/catalog')
         } catch (err) {
             console.log(err.message)
         }
     }
 
+    const abordDelete =()=> {
+        setShowModal(false)
+    }
+
+    const onDeleteAction = () => {
+        setShowModal(true);
+
+
+    }
+
     return (
+
         <div className="d-flex justify-content-center my-4">
             <Link to={`/catalog/edit/${postId}`} className="read_more_blog  mr-4">Edit</Link>
-            <a onClick={deleteHandler} className="read_more_blog  mr-4">Delete</a>
+            <a onClick={onDeleteAction} className="read_more_blog  mr-4">Delete</a>
+            {showModal && <div className="modal-backdrop">
+                <div className="modal-content">
+
+                    <div className="errorContainer">
+                        <p>Are you sure you want to proceed?</p>
+                        <button onClick={abordDelete} className="btn-no">No</button>
+                        <button onClick={deleteHandler} className="btn-yes" >Yes</button>
+                    </div>
+                </div>
+            </div> }
         </div>
     )
 }
